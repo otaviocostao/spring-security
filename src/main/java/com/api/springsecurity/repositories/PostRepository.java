2200c515121ea78b1376807828e18ec77a752588
otaviocostao <dev.otaviocostao@gmail.com>
2025-07-10 18:51:20 -0300

    Feat: Repositories

---
+++ src/main/java/com/api/springsecurity/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.api.springsecurity.repositories;
+
+import com.api.springsecurity.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
